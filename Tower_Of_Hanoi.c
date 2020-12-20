27b947bf6224e99c140bd69b9c48e831991df37e
SHUBHAM KUMAR VISHNOI <65655892+shubhamvishnoi3288@users.noreply.github.com>
2020-12-19 22:09:04 -0800

    TOH PROBLEM 3 DISC PROGRAM

---
+++ Tower_Of_Hanoi.c
@@ -0,0 +1,40 @@
+            ******************************* Algorithm: ********************************
+      Tower_Of_hanoi (disc,source,destination,spare)
+         1. if disc==1
+         2 . move disc from source to destination
+         3. else:
+         4.Tower_Of_hanoi(disc-1,source,space,destination)
+         5. move disc from source to destination
+         6. Tower_Of_hanoi(disc-1,spare,destination,source)
+                       ********************** Program*************************
+    #include<stdio.h>
+      long int static count=0;
+      void TOH(int n,char A,char B,char C)
+     {
+      if(n==1)
+   {
+      count++;
+      printf("\nMove %c to %c",A,B);
+      count++;
+   }
+      else
+     {
+      TOH(n-1,A,C,B);
+      count++;
+      printf("\nMove %c to %c",A,B);
+      count++;
+      TOH(n-1,C,B,A);
+      count++;
+   }
+     }
+      int main()
+      {
+     int n = 0;
+     char A='A',B='B',C='C';
+     printf("Enter the number of disc:");
+     scanf("%d",&n);
+     TOH(n,A,B,C);
+     printf("\nFor n=%d no. of counts are=%ld",n,count);
+     printf("\n");
+     return 0;
+    }
